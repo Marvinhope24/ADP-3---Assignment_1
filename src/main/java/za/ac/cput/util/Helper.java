f4189d431bb177fc94ee171cfd8d088e89196a27
siyabonga-cput <siyabongamasango@gmail.com>
2022-03-31 14:39:51 +0200

    added the domain,factory and helper to the admin and sport classes

---
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -0,0 +1,17 @@
+package za.ac.cput.util;
+
+//Siyabonga Tumelo Masango
+//219200815
+//31/03/2022
+//Helper.java
+public class Helper {
+
+    public static boolean isNotEmpty(String s){
+
+        return(s== null || s.isEmpty()|| s.equals("")|| s.equalsIgnoreCase("null"));
+    }
+
+
+
+
+}
